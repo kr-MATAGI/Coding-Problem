ae0ac52a7767a8a066914fb197733764b9314231
Jae Hoon <dev_matagi@kakao.com>
2020-08-31 14:55:17 +0900

    NFC Deleted

--- NFC/Server/Main.js
+++
@@ -1,8 +0,0 @@
-console.log("----Main Start !" + " ("+ process.version + ")----");
-
-// Accepter Load
-console.log("----Accepter Load Start !----");
-var accepter = require("./Component/Accepter.js");
-
-// Create Server
-accepter.CreateServer();
